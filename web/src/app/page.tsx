df9bb8a838e337ab3d3b92be517ab0b97ff7eb63
arthurlbo <arthurlbo16@gmail.com>
2023-05-20 13:03:43 -0300

    starting web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="max-w-screen flex h-screen w-full items-center justify-center bg-zinc-900 text-4xl font-semibold text-zinc-200">
+            Hello world
+        </main>
+    );
+}

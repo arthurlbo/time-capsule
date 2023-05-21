6deea243d3cf992a1313fc3ec693c5d754c2c7ed
arthurlbo <arthurlbo16@gmail.com>
2023-05-21 18:04:13 -0300

    starting memories route

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Memories = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="">Memories</Text>
+        </View>
+    );
+};
+
+export default Memories;

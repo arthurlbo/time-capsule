76106b3a6b2361239180b138db8869e41b51ba5a
arthurlbo <arthurlbo16@gmail.com>
2023-05-21 02:00:31 -0300

    adding cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
+
+app.register(cors, {
+    origin: true,
+});
+
+app.register(memoriesRoutes);
 
 app.listen({
     port: 3333,
 }).then(() => console.log("🔥 HTTP server running on http://localhost:3333"));
-
-app.get("/", async () => {
-    const users = await prisma.user.findMany();
-    return users;
-});

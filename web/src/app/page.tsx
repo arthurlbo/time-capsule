1cfc2436a046b1805d7959ffa15e166a520fc29f
arthurlbo <arthurlbo16@gmail.com>
2023-05-21 16:44:48 -0300

    create profile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from "next/headers";
+
 import { EmptyMemories } from "@/components/EmptyMemories";
 import { Copyright } from "@/components/Copyright";
+import { Profile } from "@/components/Profile";
 import { SignIn } from "@/components/SignIn";
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
+    const isAuthenticated = cookies().has("token");
+
     return (
         <main className="grid h-screen grid-cols-2">
             {/* left */}
@@ -12,7 +17,7 @@ export default function Home() {
                 <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
                 {/* stripes */}
                 <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-                <SignIn />
+                {isAuthenticated ? <Profile /> : <SignIn />}
                 <Hero />
                 <Copyright />
             </div>
---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import { getUser } from "@/lib/auth";
+
+export const Profile = () => {
+    const { avatarUrl, name } = getUser();
+
+    return (
+        <div className="flex items-center gap-3 text-left">
+            <Image src={avatarUrl} alt="user avatar" width={40} height={40} className="h-10 w-10 rounded-full" />
+            <p className="max-w-[140px] text-sm leading-snug">
+                {name}
+                <a href="" className="block text-red-400 hover:text-red-300">Quero sair</a>
+            </p>
+        </div>
+    );
+};

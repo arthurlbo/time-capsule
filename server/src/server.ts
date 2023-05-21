507432643a657f1325f1d6c2215a2f6fe0eb8524
arthurlbo <arthurlbo16@gmail.com>
2023-05-21 15:38:58 -0300

    adding jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import jwt from "@fastify/jwt";
+
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 
@@ -9,6 +13,11 @@ app.register(cors, {
     origin: true,
 });
 
+app.register(jwt, {
+    secret: "spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.listen({

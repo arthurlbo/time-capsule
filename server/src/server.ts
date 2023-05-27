80fc525761e2408d86c7092574f81b47c40baa0a
arthurlbo <arthurlbo16@gmail.com>
2023-05-27 17:28:23 -0300

    adding multipart and static package

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
+import { resolve } from "node:path";
 
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 
-import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+    root: resolve(__dirname, "../uploads"),
+    prefix: "uploads",
+});
 
 app.register(cors, {
     origin: true,
@@ -18,6 +28,7 @@ app.register(jwt, {
 });
 
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 app.listen({

7f63edb4ea55fa577f35ae3b80533132674dd6ac
arthurlbo <arthurlbo16@gmail.com>
2023-05-28 12:36:49 -0300

    creating media picker component

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+export const MediaPicker = () => {
+    const [preview, setPreview] = useState<string | null>(null);
+
+    const onFileSelected = (event: ChangeEvent<HTMLInputElement>) => {
+        const { files } = event.target;
+        if (!files) {
+            return;
+        }
+        const previewURL = URL.createObjectURL(files[0]);
+
+        setPreview(previewURL);
+    };
+
+    return (
+        <>
+            <input name="coverUrl" type="file" id="media" accept="image/*" className="invisible h-0 w-0" onChange={onFileSelected} />
+            {preview && <img src={preview} alt="" className="aspect-video w-full rounded-lg object-cover" />}
+        </>
+    );
+};

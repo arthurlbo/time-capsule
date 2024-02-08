09cf602429c1c852aa18bb6e4b92e18335357bb9
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-02-08 19:33:59 -0300

    fixing image ip domain

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["avatars.githubusercontent.com", "192.168.1.3"],
+        domains: ["avatars.githubusercontent.com", "192.168.15.117"],
     },
 };
 
